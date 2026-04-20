06e9ce499af409c32150a5ec642fa2f81e90f627
ayushmaanganguli64-code <ayushmaanganguli64@gmail.com>
2026-04-20 13:55:16 +0530

    UC1 code

---
+++ TicTacToe/src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    // Initialize all cells with '-'
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    // Print the board in grid format
+    static void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println(); // move to next line after each row
+        }
+    }
+}
