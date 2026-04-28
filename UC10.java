cba30a8ef9b25d5c257daf0124f14d8ade53c391
Ayushmaan Ganguli <ayushmaanganguli64@gmail.com>
2026-04-29 00:25:21 +0530

    UC10

---
+++ UC10.java
@@ -0,0 +1,28 @@
+
+public class TicTacToe {
+
+    static char[][] board = {
+        {'X', 'O', 'X'},
+        {'O', 'X', 'O'},
+        {'O', 'X', 'O'}
+    };
+
+    
+    public static void main(String[] args) {
+        System.out.println("Is Draw: " + isDraw());
+    }
+
+   
+    static boolean isDraw() {
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {  
+                    return false;
+                }
+            }
+        }
+
+        return true; 
+    }
+}
