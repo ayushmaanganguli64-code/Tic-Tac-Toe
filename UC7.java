780328e6a0c4426b18ff1c8e289b9e53d2fd0189
Ayushmaan Ganguli <ayushmaanganguli64@gmail.com>
2026-04-29 00:16:22 +0530

    UC7

---
+++ UC7.java
@@ -0,0 +1,44 @@
+public class TicTacToe {
+
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        computerMove('O');
+        printBoard();
+    }
+
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row <= 2 && col >= 0 && col <= 2 && board[row][col] == '-';
+    }
+
+    static void placeMove(int row, int col, char symbol) {
+        if (isValidMove(row, col)) {
+            board[row][col] = symbol;
+        }
+    }
+
+    static void computerMove(char symbol) {
+        int row, col;
+
+        do {
+            int move = (int)(Math.random() * 9); // 0–8
+            row = move / 3;
+            col = move % 3;
+        } while (!isValidMove(row, col));
+
+        placeMove(row, col, symbol);
+    }
+
+    static void printBoard() {
+        for (char[] row : board) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
